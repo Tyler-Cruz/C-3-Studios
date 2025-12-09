44b6fceaca00aae1778e222fc146eb7a57e8eea2
Tyler Cruz <cruztb@merrimack.edu>
2025-12-09 15:08:44 -0500

    adding proper port number

--- api.py
+++ api.py
@@ -121,4 +121,4 @@ def analyze():
 
 if __name__ == "__main__":
     print("Server starting on http://127.0.0.1:5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=49164, debug=True)
